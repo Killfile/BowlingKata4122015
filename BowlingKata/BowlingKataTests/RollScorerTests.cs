f0422d9dc1687bb49f7b98a5c61c489c13a130ad
Killfile <thegreatkillfile@gmail.com>
2015-04-13 21:20:56 -0400

    Refactored to reflect 10 frame game

---
+++ BowlingKata/BowlingKataTests/RollScorerTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BowlingKata;
+using NUnit.Framework;
+namespace BowlingKata.Tests
+{
+    [TestFixture()]
+    public class RollScorerTests
+    {
+        [Test()]
+        public void RollScorerCanBeConstructed()
+        {
+            new RollScorer(string.Empty);
+        }
+
+        [Test]
+        public void RollScoreHasScoreMethod()
+        {
+            var scorer = new RollScorer("-- -- -- -- -- -- -- -- -- --");
+            int score = scorer.GetScore();
+        }
+
+        private int GetScore(string rolls) {
+            var scorer = new RollScorer(rolls);
+            return scorer.GetScore();
+        }
+
+        [Test]
+        [TestCase("-- -- -- -- -- -- -- -- -- --",0)]
+        [TestCase("1- -- -- -- -- -- -- -- -- --", 1)]
+        [TestCase("11 -- -- -- -- -- -- -- -- --",2)]
+        [TestCase("-1 -- -- -- -- -- -- -- -- --",1)]
+        [TestCase("X  -- -- -- -- -- -- -- -- --", 10)]
+        [TestCase("X  11 11 -- -- -- -- -- -- --",18)]
+        [TestCase("X  X  X  -- -- -- -- -- -- --",60)]
+        [TestCase("-- -- -- -- -- -- -- -- -- X  X  X", 30)]
+        [TestCase("X X X X X X X X X X X X", 300)]
+        public void RollGivesExpectedScore(string roll, int expectedScore)
+        {
+            Assert.That(GetScore(roll), Is.EqualTo(expectedScore));
+        }
+    }
+}
---
+++ BowlingKata/RollScorer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingKata
+{
+    public class RollScorer
+    {
+        private string _rolls;
+        public RollScorer(string rolls)
+        {
+            _rolls = rolls;
+        }
+
+        public int GetScore()
+        {
+            
+            int accumulator = 0;
+            var tokenizedRolls = _rolls.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < 10; i++ )
+            {
+                var rollScore = ScoreRoll(tokenizedRolls, i);
+                accumulator += rollScore;
+            }
+
+            return accumulator;
+        }
+
+        private static int ScoreRoll(string[] tokenizedRolls, int i)
+        {
+            
+            if (tokenizedRolls[i].IsStrike())
+                return ScoreStrikeBonus(tokenizedRolls, i);
+            return GetRawRollScore(tokenizedRolls, i);
+        }
+
+        private static int GetRawRollScore(string[] tokenizedRolls, int i)
+        {
+            
+            if (i >= tokenizedRolls.Length)
+                return 0;
+            if (tokenizedRolls[i].IsStrike())
+                return 10;
+            return ScoreStandardRoll(tokenizedRolls[i]);
+            
+        }
+
+        private static int ScoreStandardRoll(string roll)
+        {
+            int accumulator = 0;
+            foreach (char c in roll)
+            {
+                int result = 0;
+                if(int.TryParse(c.ToString(), out result))
+                    accumulator += result;
+            }
+            
+            return accumulator;
+        }
+
+        private static int ScoreStrikeBonus(string[] tokenizedRolls, int i)
+        {
+            return 
+                GetRawRollScore(tokenizedRolls, i) +
+                GetRawRollScore(tokenizedRolls, i + 1) +
+                GetRawRollScore(tokenizedRolls, i + 2);
+                
+        }
+    }
+
+    public static class StringExtensions {
+        public static bool IsStrike(this string x) {
+            return x == "X";
+        }
+    }
+}
